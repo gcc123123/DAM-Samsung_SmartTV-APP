70af05f00e44255ea60066ce6ffe1ebb425dfeed
Johan D. Rohan <johanderohan@hotmail.com>
2015-04-11 12:13:36 +0200

    API Login

--- app/scenes/SceneLogin.js
+++ app/scenes/SceneLogin.js
@@ -9,6 +9,7 @@ SceneSceneLogin.prototype.initialize = function () {
 	// this function will be called only once when the scene manager show this scene first time
 	// initialize the scene controls and styles, and initialize your variables here
 	// scene HTML and CSS will be loaded before this function is called
+	var username = '';
 	$('#password').sfTextInput({
 		text:'',
 		maxlength:30,
@@ -16,6 +17,46 @@ SceneSceneLogin.prototype.initialize = function () {
 		{
 	        if (text) {
 	        	password = text;
+
+	        	$.ajax({
+				  type: "GET",
+				  crossDomain: true,
+				  async: true,
+				  dataType: "json",
+				  url: API+"/authentication/token/new",
+				  data: { api_key: api_key },
+				  success: function(data){
+				  	var request_token = data.request_token;
+
+				  	$.ajax({
+					  type: "GET",
+					  crossDomain: true,
+					  async: true,
+					  dataType: "json",
+					  url: API+"/authentication/token/validate_with_login",
+					  data: { api_key: api_key, request_token: request_token, username: username, password: password },
+					  success: function(data){
+
+					  		$.ajax({
+							  type: "GET",
+							  crossDomain: true,
+							  async: true,
+							  dataType: "json",
+							  url: API+"/authentication/session/new",
+							  data: { api_key: api_key, request_token: request_token },
+							  success: function(data){
+
+							  	session_id = data.session_id;
+				    			alert(session_id);
+							  	
+							  }
+							});
+					  	
+					  }
+					});
+
+				  }
+				});
 	        }
 	        else {
 	        	//error
@@ -28,6 +69,7 @@ SceneSceneLogin.prototype.initialize = function () {
 		oncomplete: function (text) 
 		{
 	        if (text) {
+	        	username = text;
 	        	$('#password').sfTextInput('focus');
 	        	$('#password').sfTextInput('setKeypadPos', 450, 170);
 	        }
